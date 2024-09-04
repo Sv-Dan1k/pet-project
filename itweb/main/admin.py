61ce83c85b23569d092963d0f9e56115847f3e85
Sv-Dan1k <164805682+Sv-Dan1k@users.noreply.github.com>
2024-09-05 02:16:37 +0300

    Delete itweb directory

--- itweb/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Articles, Author, Tag, Quote
-
-
-admin.site.register(Articles)
-admin.site.register(Author)
-admin.site.register(Tag)
-admin.site.register(Quote)
-
--- itweb/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('news', views.news, name='news'),
-    path('authors', views.authors, name='authors'),
-    path('quotes',views.quotes, name='quotes'),
-]
